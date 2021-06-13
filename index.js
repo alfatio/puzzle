e5bc5d61baed8e1baf396eb65d88c1f777b11980
alfatio <alfatioutama@gmail.com>
2021-06-13 18:23:44 +0700

    add_changeNum_func

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const limitUpper = 562041,
       limitLower = 108457
 
 
-
 function hasTwinAdjacent(arrStr = ""){
+  // check has twin adjacent number
 
   for(let i = 0; i < arrStr.length - 1; i++){
     if(arrStr[i] === arrStr[i + 1]){
@@ -17,6 +17,7 @@ function hasTwinAdjacent(arrStr = ""){
 }
 
 function checkRightHigh(arrStr = ""){
+  // check number to the right always higher
 
   for(let i = 0; i < arrStr.length - 1; i++){
     if(Number(arrStr[i]) > Number(arrStr[i + 1])){
@@ -27,18 +28,44 @@ function checkRightHigh(arrStr = ""){
   return true
 }
 
+function changeNumber(arrStr = ""){
+  // to skip unnecessary number
+
+  for(let i = 0; i < arrStr.length - 1; i++){
+    if(Number(arrStr[i]) > Number(arrStr[i + 1])){
+
+      let newStr = arrStr.split('')
+
+      for(let j = i; j < newStr.length; j++){
+        newStr[j] = newStr[i]
+      }
+
+      arrStr = newStr.join('')
+      break
+    }
+  }
+  return arrStr
+}
+
 function countPassword(limitLower,limitUpper){
 
   let countOutput = 0
 
   for(let i = limitLower; i < limitUpper; i++){
+
     let numberToCheck = i.toString()
-    let hasTwin = hasTwinAdjacent(numberToCheck)
-    if(hasTwin){
-      let rightAlwaysHigher = checkRightHigh(numberToCheck)
-      if(rightAlwaysHigher){
+    let rightAlwaysHigher = checkRightHigh(numberToCheck)
+    
+    if(rightAlwaysHigher){
+      let hasTwin = hasTwinAdjacent(numberToCheck)
+
+      if(hasTwin){
         countOutput++
       }
+      
+    }else{
+      let newNumber = +changeNumber(numberToCheck)
+      i = newNumber - 1
     }
   }
 
